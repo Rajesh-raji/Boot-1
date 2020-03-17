51b16ca84e712f3d489503ce270f4793abc3d25d
Samsung <Samsung@Samsung-PC>
2020-03-17 23:01:49 +0530

    first changes

---
+++ Boot-1/src/main/java/com/sathya/service/DemoService.java
@@ -0,0 +1,13 @@
+package com.sathya.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DemoService {
+	
+	public String getService() {
+		
+		return "raj";
+	}
+
+}
---
+++ Boot-1/src/test/java/com/sathya/test/TestdemoService.java
@@ -0,0 +1,15 @@
+package com.sathya.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import com.sathya.service.DemoService;
+
+public class TestdemoService {
+	
+	@Test
+	public void testGetService() {
+		DemoService ds=new DemoService();
+		Assertions.assertEquals("raj",ds.getService());
+	}
+
+}
